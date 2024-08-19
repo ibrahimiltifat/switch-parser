cc0d5db2dda12c6ddfbf46aa2f85b9a0ca624f83
asgharzady <asgharzady@gmail.com>
2024-08-19 16:54:02 +0500

    code refactor

--- src/main/kotlin/com/example/demo/service/TransactionService.kt
+++ src/main/kotlin/com/example/demo/service/TransactionService.kt
@@ -16,7 +16,7 @@ class TransactionService(
 
     fun parser(json: String, channelId: Long): List<Map<String, Any?>> {
         val transactions = getTransactionsForChannel(channelId) // Get transactions based on channelId
-        val deserializedData = dynamicDeserialize(Json.parseToJsonElement(json).jsonObject, transactions)
+        val deserializedData = dynamicDeserialize(json, transactions)
         val response = mutableListOf<Map<String, Any?>>()
         deserializedData.forEach { (key, value) ->
             println("$key: $value")
@@ -32,144 +32,114 @@ class TransactionService(
         return transactionRepository.findAllById(transactionIds)
     }
 
-    fun dynamicDeserialize(jsonObject: JsonObject, transactions: List<Transaction>): Map<String, Any?> {
+    fun dynamicDeserialize(json: String, transactions: List<Transaction>): Map<String, Any?> {
+        val jsonElement = Json.parseToJsonElement(json).jsonObject
         val result = mutableMapOf<String, Any?>()
 
         transactions.forEach { transaction ->
             val key = transaction.key
             val type = transaction.type
 
-            when (type) {
-                "object" -> {
-                    if (jsonObject[key] != null) {
-                        val map = getObjOrArrayValue(jsonObject[key]!!.toString(), transaction.id)
-                        map.forEach { (mapKey, mapValue) ->
-                            result["$key.$mapKey"] = mapValue
-                        }
+            if (type == "object") {
+                if (jsonElement[key] != null) {
+                    val map = getObjValue(jsonElement[key].toString(), transaction.id)
+                    for (m in map) {
+                        result[key + "." + m.key] = m.value
                     }
-                    else{
-                        var list = getKeys(transaction)
-                        for (l in list){
-                            result[l] = null
-                        }
+                } else {
+                    var list = getKeys(transaction)
+                    for (l in list) {
+                        result[l] = null
                     }
                 }
-
-                "array" -> {
-                    if (jsonObject[key] != null) {
-                        val array = getObjOrArrayValue(jsonObject[key]!!.toString(), transaction.id)
-                        array.forEach { (arrayKey, arrayValue) ->
-                            result["$key$arrayKey"] = arrayValue
+            } else if (type == "array") {
+                val list = jsonElement[key]?.jsonArray
+                if (list != null) {
+                    val arrayDetail = detailRepository.findByTransactionKey(transaction).transactionVal
+                    val arrayDetailMapping = detailRepository.findAllByTransactionKey(arrayDetail)
+                    for ((index, l) in list.withIndex()) {
+                        if (arrayDetailMapping.isEmpty()) {
+                            val transactionType = transactionRepository.findById(arrayDetail.id).get().type
+                            result[key + index] = getJsonPrimitiveValue(transactionType, l.jsonPrimitive)
+                        } else {
+                            val map = getObjValue(l.toString(), arrayDetail.id)
+                            for (m in map) {
+                                result[key + index + "." + m.key] = m.value
+                            }
                         }
                     }
-                    else{
-                        var list = getKeys(transaction)
-                        for (l in list){
-                            result[l] = null
-                        }
+                } else {
+                    var list = getKeys(transaction)
+                    for (l in list) {
+                        result[l] = null
                     }
                 }
-
-                else -> {
-                    val jsonValue = jsonObject[key]?.jsonPrimitive
-                    if (jsonValue != null) {
-                        result[key] = when (type) {
-                            "String" -> jsonValue.content
-                            "Long" -> jsonValue.longOrNull
-                            "Int" -> jsonValue.intOrNull
-                            "Boolean" -> jsonValue.booleanOrNull
-                            else -> null
-                        }
-                    } else {
-                        result[key] = null
-                    }
+            } else {
+                val jsonValue = jsonElement[key]?.jsonPrimitive
+                if (jsonValue != null) {
+                    result[key] = getJsonPrimitiveValue(type, jsonValue)
+                } else {
+                    result[key] = null
                 }
             }
         }
         return result
     }
 
-    fun getKeys(transaction: Transaction): List<String>{
+    fun getJsonPrimitiveValue(type: String, jsonValue: JsonPrimitive) =
+        when (type) {
+            "String" -> jsonValue.content
+            "Long" -> jsonValue.longOrNull
+            "Int" -> jsonValue.intOrNull
+            "Boolean" -> jsonValue.booleanOrNull
+            else -> null
+        }
+
+
+    fun getObjValue(json: String, id: Long): Map<String, Any?> {
+        val detailList = detailRepository.findAllByTransactionKey(transactionRepository.findById(id).get())
+        var transactions: MutableList<Transaction> = mutableListOf()
+        val jsonElement = Json.parseToJsonElement(json)
+        if (detailList.isEmpty()) {
+            transactions.add(transactionRepository.findById(id).get())
+
+
+        } else {
+            for (detail in detailList) {
+                transactions.add(transactionRepository.findById(detail.transactionVal.id).get())
+            }
+        }
+        println("calling parser")
+        val result = dynamicDeserialize(jsonElement.toString(), transactions)
+        return result
+    }
+
+    fun getKeys(transaction: Transaction): List<String> {
         var respose = ""
         var list = mutableListOf<String>()
         var details = detailRepository.findAllByTransactionKey(transaction)
-        var transactions= details.map { it.transactionVal }
+        var transactions = details.map { it.transactionVal }
 
-        for(trx in transactions){
-            if(trx.type != "object" && trx.type != "array"){
-                if(transaction.type == "object")
+        for (trx in transactions) {
+            if (trx.type != "object" && trx.type != "array") {
+                if (transaction.type == "object")
                     respose = "${transaction.key}.${trx.key}"
                 else
                     respose = "${transaction.key}0.${trx.key}"
                 list.add(respose)
-            }
-            else{
+            } else {
                 val internalList = getKeys(trx)
-                for(il in internalList){
-                    if(transaction.type == "object")
+                for (il in internalList) {
+                    if (transaction.type == "object")
                         list.add("${transaction.key}.$il")
-                    else{
+                    else {
                         list.add("${transaction.key}0.$il")
                     }
                 }
             }
 
         }
-        return  list
-    }
-    fun getObjOrArrayValue(json: String, id: Long): Map<String, Any?> {
-        val detailList = detailRepository.findAllByTransactionKey(transactionRepository.findById(id).get()) //8
-        val transactions = detailList.map { it.transactionVal }
-        val jsonElement = Json.parseToJsonElement(json)
-        return if (jsonElement is JsonArray) {
-            if (transactions.get(0).type == "String")
-                parseArray(jsonElement, listOf(transactions.get(0)))
-            else {
-                val trx = detailRepository.findAllByTransactionKey(transactions.get(0))
-                parseArray(jsonElement, trx.map { it.transactionVal })
-            }
-        } else {
-            dynamicDeserialize(jsonElement.jsonObject, transactions)
-        }
+        return list
     }
 
-    fun parseArray(jsonArray: JsonArray, transactions: List<Transaction>): Map<String, Any?> {
-        val arrayResult = mutableMapOf<String, Any?>()
-        jsonArray.forEachIndexed { index, element ->
-            when (element) {
-                is JsonObject -> {
-                    val map = dynamicDeserialize(element, transactions)
-                    map.forEach { (mapKey, mapValue) ->
-                        arrayResult["$index.$mapKey"] = mapValue
-                    }
-                }
-
-                is JsonArray -> {
-                    val nestedArray = parseArray(element, transactions)
-                    nestedArray.forEach { (nestedKey, nestedValue) ->
-                        arrayResult["$index.$nestedKey"] = nestedValue
-                    }
-                }
-
-                else -> {
-                    if (transactions.size == 1) {
-                        val type = transactions.get(0).type
-                        val jsonValue = element.jsonPrimitive
-                        arrayResult["$index"] = when (type) {
-                            "String" -> jsonValue.content
-                            "Long" -> jsonValue.longOrNull
-                            "Int" -> jsonValue.intOrNull
-                            "Boolean" -> jsonValue.booleanOrNull
-                            else -> null
-                        }
-                    }
-                }
-//                else -> {
-//                    arrayResult["$index"] = element.jsonPrimitive.contentOrNull
-//                }
-            }
-        }
-
-        return arrayResult
-    }
 }
